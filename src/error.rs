f496b46da9a584fca86264a742b5ab346f22f0f4
Dokkae6949 <finnliry@gmail.com>
2024-04-19 19:26:45 +0200

    Implement image url scraping

--- src/error.rs
+++ src/error.rs
@@ -2,4 +2,15 @@
 pub enum Error {
     #[error("generic error: {0}")]
     Generic(String),
+
+    #[error("builder failed: {0}")]
+    BuilderError(#[from] derive_builder::UninitializedFieldError),
+
+    #[error("network error: {0}")]
+    NetworkError(#[from] reqwest::Error),
+
+    #[error("failed to parse url: {0}")]
+    UrlParseError(#[from] url::ParseError),
 }
+
+pub type Result<T> = std::result::Result<T, Error>;
